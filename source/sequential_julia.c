a2e52d3a7b0f0db4626af244a62adfbe36b6c833
Pablo Pavan <adeliaopavan@gmail.com>
2018-03-27 16:03:43 -0300

    Add sequential code and Makefile

---
+++ source/sequential_julia.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tempo.h"
+#include <math.h>
+
+
+
+int write_bmp_header(FILE *f, int width, int height) {
+
+  unsigned int row_size_in_bytes = width * 3 + 
+	  ((width * 3) % 4 == 0 ? 0 : (4 - (width * 3) % 4));
+
+  // Define all fields in the bmp header
+  unsigned int filesize = 54 + (int)(row_size_in_bytes * height * sizeof(char));
+  short reserved[2] = {0,0};
+  unsigned int offset = 54;
+
+  unsigned int size = 40;
+  char id[2] = "BM";
+  unsigned short planes = 1;
+  unsigned short bits = 24;
+  unsigned int compression = 0;
+  unsigned int image_size = width * height * 3 * sizeof(char);
+  int x_res = 0;
+  int y_res = 0;
+  unsigned int ncolors = 0;
+  unsigned int importantcolors = 0;
+
+	
+  // Write the bytes to the file, keeping track of the
+  // number of written "objects"
+  size_t ret = 0;
+  ret += fwrite(id, sizeof(char), 2, f);
+  ret += fwrite(&filesize, sizeof(int), 1, f);
+  ret += fwrite(reserved, sizeof(short), 2, f);
+  ret += fwrite(&offset, sizeof(int), 1, f);
+  ret += fwrite(&size, sizeof(int), 1, f);
+  ret += fwrite(&width, sizeof(int), 1, f);
+  ret += fwrite(&height, sizeof(int), 1, f);
+  ret += fwrite(&planes, sizeof(short), 1, f);
+  ret += fwrite(&bits, sizeof(short), 1, f);
+  ret += fwrite(&compression, sizeof(int), 1, f);
+  ret += fwrite(&image_size, sizeof(int), 1, f);
+  ret += fwrite(&x_res, sizeof(int), 1, f);
+  ret += fwrite(&y_res, sizeof(int), 1, f);
+  ret += fwrite(&ncolors, sizeof(int), 1, f);
+  ret += fwrite(&importantcolors, sizeof(int), 1, f);
+
+  // Success means that we wrote 17 "objects" successfully
+  return (ret != 17);
+}
+
+
+
+int compute_julia_pixel(int x, int y, int width, int height, float tint_bias, unsigned char *rgb) {
+
+  // Check coordinates
+  if ((x < 0) || (x >= width) || (y < 0) || (y >= height)) {
+    fprintf(stderr,"Invalid (%d,%d) pixel coordinates in a %d x %d image\n", x, y, width, height);
+    return -1;
+  }
+
+  
+  // "Zoom in" to a pleasing view of the Julia set
+  float X_MIN = -1.6, X_MAX = 1.6, Y_MIN = -0.9, Y_MAX = +0.9;
+  float float_y = (Y_MAX - Y_MIN) * (float)y / height + Y_MIN ;
+  float float_x = (X_MAX - X_MIN) * (float)x / width  + X_MIN ;
+
+  // Point that defines the Julia set
+  float julia_real = -.79;
+  float julia_img = .15;
+
+  // Maximum number of iteration
+  int max_iter = 300;
+
+  // Compute the complex series convergence
+  float real=float_y, img=float_x;
+  int num_iter = max_iter;
+  while (( img * img + real * real < 2 * 2 ) && ( num_iter > 0 )) {
+    float xtemp = img * img - real * real + julia_real;
+    real = 2 * img * real + julia_img;
+    img = xtemp;
+    num_iter--;
+  }
+
+  // Paint pixel based on how many iterations were used, using some funky colors
+  float color_bias = (float) num_iter / max_iter;
+  rgb[0] = (num_iter == 0 ? 200 : - 500.0 * pow(tint_bias, 1.2) *  pow(color_bias, 1.6));
+  rgb[1] = (num_iter == 0 ? 100 : -255.0 *  pow(color_bias, 0.3));
+  rgb[2] = (num_iter == 0 ? 100 : 255 - 255.0 * pow(tint_bias, 1.2) * pow(color_bias, 3.0));
+
+  return 0;
+}
+
+
+// main
+
+int main(int argc, char *argv[]){
+        
+  if(argc == 1) { 
+    printf("\n ./set_julia.exec TAM FILE \n");
+    exit(0);
+  }
+	
+  int x = 0;
+  int y = 0;
+  int size = 0;
+    
+  FILE *input = fopen(argv[2], "w");
+  if (input == NULL) {
+    fprintf(stderr, "Can't open output file %s!\n",argv[2]);
+    exit(1);
+  }
+
+  size = atoi(argv[1]);
+  
+  unsigned char *rgb = malloc(sizeof(unsigned char) * (size*(2*size)*3));
+	
+  tempo1();
+  //Calculo dos pixels
+  for(y=0;y<size;y++){
+    for(x=0;x<size*2;x++){
+	compute_julia_pixel(x ,y ,size*2 ,size ,1.0 ,&rgb[y*3*size*2+x*3]);
+    }
+  }   
+  tempo2();
+      
+  int return_validation =  write_bmp_header(input,size*2,size);
+  if(!return_validation){
+    printf("Successful Execution");
+  }else{
+    printf("PANIC! Error in write bmp");
+  }
+  fwrite(rgb, sizeof(unsigned char),(size*(2*size)*3) , input);
+  fclose(input);
+  tempoFinal("mili segundos", argv[0], MSGLOG);
+  return 0;
+}
