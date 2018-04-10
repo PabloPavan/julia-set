d402191d498dcfc0b60cafb3fa9a9d57c8ad22eb
Pablo Pavan <adeliaopavan@gmail.com>
2018-04-10 09:33:39 -0300

    add csv results

--- source/run_seq.sh
+++ source/run_seq.sh
@@ -11,10 +11,10 @@ if [ ! $# -ge 4 ]
 	exit 1
 fi
 		
-echo "step; value" > $1_$2_$4.csv
+echo "step; value" > ../results/$1_$2_$4.csv
 for step in $(seq "$4"); do
 	./$1 $2 $3 &> tmp
-	echo "$step"";" `cat tmp | grep "Tempo total=" | cut -d '=' -f2 | sort -n | tail -n1 | sed 's/ms//g' | sed 's/ //g'` >> $1_$2_$4.csv
+	echo "$step"";" `cat tmp | grep "Tempo total=" | cut -d '=' -f2 | sort -n | tail -n1 | sed 's/ms//g' | sed 's/ //g'` >> ../results/$1_$2_$4.csv
 	echo "step : $step"
 	sleep 10
 done
